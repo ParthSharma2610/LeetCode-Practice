ed75d0b9a38edfc01d3a6e825cfabb99828d116c
ParthSharma2610 <parthsharma902427@gmail.com>
2026-07-16 21:50:04 +0530

    Time: 59 ms (37.5%), Space: 126.2 MB (10.5%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+
+        int[] mx = new int[n];
+        int prefixMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; ++i) {
+            prefixMax = Math.max(prefixMax, nums[i]);
+            mx[i] = prefixMax;
+        }
+
+        int[] prefixGcd = new int[n];
+        for (int i = 0; i < n; ++i) {
+            prefixGcd[i] = gcd(nums[i], mx[i]);
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long ans = 0;
+        int left = 0,
+            right = n - 1;
+        while (left < right) {
+            ans += gcd(prefixGcd[left], prefixGcd[right]);
+            ++left;
+            --right;
+        }
+
+        return ans;
+    }
+
+    public int gcd(int a, int b) {
+        if(b == 0){
+            return a;
+        }
+        return gcd(b,a % b);
+    }
+}
